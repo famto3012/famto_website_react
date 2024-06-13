d8f4eba1240b9042408fef40fee3d71932038498
Gowri-US <gowrikunju1805@gmail.com>
2024-06-13 15:40:14 +0530

    privacy and policies

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import TermsAndConditions from './terms&conditions/TermsAndConditions.jsx'
+import FAQ from './faq/FAQ.jsx'
+import CancellationAndRefundPolicy from './cancellationandrefundpolicy/CancellationAndRefundPolicy.jsx'
+import PrivacyPolicy from './privacypolicy/PrivacyPolicy.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path="/terms" element={<TermsAndConditions />} />
+      <Route path="/faq" element={<FAQ/>} />
+      <Route path="/refundpolicy" element={<CancellationAndRefundPolicy />}/>
+      <Route path="/privacy" element={<PrivacyPolicy />}/>
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
